47cb5bd1f8164a27efe2dcba6758c112bc65850b
Beau Raines <beau.raines@gmail.com>
2021-07-22 10:14:01 -0700

    Updates risk guage colors
    Decreasing risk is now shown as green for positive, increasing risk in red

--- plotlyGuage.R
+++ plotlyGuage.R
@@ -36,23 +36,23 @@ fig
 
 
 
-fig <- plot_ly(
-  domain = list(x = c(0, 1), y = c(0, 1)),
-  value = 450,
-  title = list(text = "Speed"),
-  type = "indicator",
-  mode = "gauge+number+delta",
-  delta = list(reference = 380),
-  gauge = list(
-    axis =list(range = list(NULL, 500)),
-    steps = list(
-      list(range = c(0, 250), color = "lightgray"),
-      list(range = c(250, 400), color = "gray")),
-    threshold = list(
-      line = list(color = "red", width = 4),
-      thickness = 0.75,
-      value = 490))) 
-fig <- fig %>%
-  layout(margin = list(l=20,r=30))
-
-fig
+# fig <- plot_ly(
+#   domain = list(x = c(0, 1), y = c(0, 1)),
+#   value = 450,
+#   title = list(text = "Speed"),
+#   type = "indicator",
+#   mode = "gauge+number+delta",
+#   delta = list(reference = 380),
+#   gauge = list(
+#     axis =list(range = list(NULL, 500)),
+#     steps = list(
+#       list(range = c(0, 250), color = "lightgray"),
+#       list(range = c(250, 400), color = "gray")),
+#     threshold = list(
+#       line = list(color = "red", width = 4),
+#       thickness = 0.75,
+#       value = 490))) 
+# fig <- fig %>%
+#   layout(margin = list(l=20,r=30))
+# 
+# fig
