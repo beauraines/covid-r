d691b6fc96de1bdfb545e9eab785bad7195d1d7e
Beau Raines <beau.raines@gmail.com>
2021-08-21 13:03:30 -0700

    Hospitalization notebook
    * Adds a hospitalizations notebook to explore that data
    * Updates existing `hospitalizations.R`
    * exlcudes PDFs from the repository

--- hospitalizations.R
+++ hospitalizations.R
@@ -2,21 +2,46 @@ library(RSocrata)
 library(tidyverse)
 hospitalizations = read.socrata("https://healthdata.gov/resource/g62h-syeh.csv")
 hospitalizations$date = as.Date(hospitalizations$date)
+hospitalizations <-tibble(hospitalizations)
 unique(hospitalizations$state)
 
 hospitalizations_wa = subset(hospitalizations,state == 'WA')
 
-beds <-ggplot(hospitalizations_wa)+
+ggplot(hospitalizations_wa)+
   #geom_line(aes(x=date,y=total_adult_patients_hospitalized_confirmed_and_suspected_covid))+
-  geom_line(aes(x=date,y=inpatient_beds_used_covid)) +
   geom_line(aes(x=date,y=inpatient_beds))
+
+
+## Hospitalizations for Covid are on the rise
+ggplot(hospitalizations_wa)+
+  geom_line(aes(x=date,y=inpatient_beds_utilization))
+
 
 
 ggplot(hospitalizations_wa)+
      #geom_line(aes(x=date,y=total_adult_patients_hospitalized_confirmed_and_suspected_covid))+
-     geom_line(aes(x=date,y=inpatient_bed_covid_utilization)) +
-     geom_line(aes(x=date,y=inpatient_beds_utilization))
+     geom_line(aes(x=date,y=inpatient_bed_covid_utilization))
 
+ggplot(hospitalizations_wa)+
+  geom_line(aes(x=date,y=inpatient_bed_covid_utilization)) + 
+  geom_line(aes(x=date,y=inpatient_beds_utilization))
+
+
+hospitalizations_wa %>%
+  filter(date == max(date)) %>%
+  select(date,
+         inpatient_beds,
+         inpatient_beds_used,
+         inpatient_beds_utilization,
+         inpatient_bed_covid_utilization,
+         percent_of_inpatients_with_covid,
+         staffed_adult_icu_bed_occupancy,
+         staffed_icu_adult_patients_confirmed_and_suspected_covid,
+         total_adult_patients_hospitalized_confirmed_and_suspected_covid)
+
+
+
+## Deaths
 ggplot(hospitalizations_wa)+
   #geom_line(aes(x=date,y=total_adult_patients_hospitalized_confirmed_and_suspected_covid))+
   geom_line(aes(x=date,y=deaths_covid))
