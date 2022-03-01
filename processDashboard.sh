5b3df5211800b631faf1104578fc8465c03f35a1
Beau Raines <beau.raines@gmail.com>
2022-02-28 20:20:49 -0800

    Modifications to use AWS
    * specifies new environment variables
    * updates instructions to build container
    * modifies upload.R to push to S3
    Requires these input arguments
    
     key = args[1]
     secret = args[2]
     region=args[3]
     bucket = args[4]

--- processDashboard.sh
+++ processDashboard.sh
@@ -5,4 +5,5 @@ wget https://raw.githubusercontent.com/beauraines/covid-r/main/CovidDashboard.Rm
 wget https://raw.githubusercontent.com/beauraines/covid-r/main/CumulativeDeathsAndCases.R
 
 Rscript -e "rmarkdown::render('CovidDashboard.Rmd')"
-Rscript upload.R $AZURE_STORAGE_ACCOUNT $AZURE_STORAGE_KEY CovidDashboard.html
+Rscript upload.R $AWS_KEY $AWS_SECRET $AWS_REGION $AWS_BUCKET
+
